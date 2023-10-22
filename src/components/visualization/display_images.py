c25d850e684c5cc6d8854ac3312674bcdc9056ea
Simeon Allmendinger <simeon.allmendinger@fim-rc.de>
2023-10-22 07:45:12 +0200

    add visualization

---
+++ src/components/visualization/display_images.py
@@ -0,0 +1,49 @@
+import torch
+import matplotlib.pyplot as plt
+
+def show_images(images, title=""):
+    """Shows the provided images as sub-pictures in a square"""
+
+    # Converting images to CPU numpy arrays
+    if type(images) is torch.Tensor:
+        images = images.detach().cpu().numpy()
+
+    # Defining number of rows and columns
+    fig = plt.figure(figsize=(8, 8))
+    rows = int(len(images) ** (1 / 2))
+    cols = round(len(images) / rows)
+
+    # Populating figure with sub-plots
+    idx = 0
+    for r in range(rows):
+        for c in range(cols):
+            fig.add_subplot(rows, cols, idx + 1)
+
+            if idx < len(images):
+                plt.imshow(images[idx][0], cmap="gray")
+                idx += 1
+    fig.suptitle(title, fontsize=30)
+
+    # Showing the figure
+    plt.show()
+    
+# Shows the first batch of images
+def show_first_batch(loader):
+    for batch in loader:
+        show_images(batch[0], "Images in the first batch")
+        break
+
+def show_forward(ddpm, loader, device):
+    # Showing the forward process
+    for batch in loader:
+        imgs = batch[0]
+
+        show_images(imgs, "Original images")
+
+        for percent in [0.25, 0.5, 0.75, 1]:
+            show_images(
+                ddpm(imgs.to(device),
+                     [int(percent * ddpm.n_steps) - 1 for _ in range(len(imgs))]),
+                f"DDPM Noisy images {int(percent * 100)}%"
+            )
+        break
